9775840b2b5f1f25a757f020ffb2cfed84c3693d
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-05-03 21:50:48 +0530

    redirect from fb fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import {RulesComponent} from './rules/rules.component';
 
 const routes: Routes = [
 {path:'',component:BasicComponent,data:{animation:'basicPage'}},
-{path:'rules',component:RulesComponent,data:{animation:'rulesPage'}},
-{path:'rules/:id',component:RulesComponent,data:{animation:'rulesIdPage'}}
+{path:'rules/:id',component:RulesComponent,data:{animation:'rulesIdPage'}},
+{path:'rules',component:RulesComponent,data:{animation:'rulesPage'}}
 ];
 
 @NgModule({
