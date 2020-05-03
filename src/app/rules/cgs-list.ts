c63c347d9377636dbaa46a72d037782a9ddc5b34
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-05-03 22:25:58 +0530

    zoomout fix and cg contact added

--- src/app/rules/cgs-list.ts
+++ src/app/rules/cgs-list.ts
@@ -4,9 +4,10 @@ export interface CG{
 	image:string;
 	description:string;
 	contact:string;
+	phone:string;
 } 
 
 export const cgs_list:CG[]=[
-{id:1,name:"Harmeet Singh",image:"assets/harmeet.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/harmeet.saluja.16"},
-{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/profile.php?id=100007865385033"},
+{id:1,name:"Harmeet Singh",image:"assets/harmeet.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/harmeet.saluja.16",phone:"7677294007"},
+{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/profile.php?id=100007865385033",phone:"+919929919437"},
 ]
