473e488e058793ebcdc15ffa4267997d177a1645
Anuj Agrawal <anujagrawal.official@gmail.com>
2020-05-05 22:20:29 +0530

    added cover images

--- src/app/rules/cgs-list.ts
+++ src/app/rules/cgs-list.ts
@@ -10,5 +10,5 @@ export interface CG{
 
 export const cgs_list:CG[]=[
 {id:1,name:"Harmeet Singh",image:"assets/harmeet.jpeg",description:"Hi I am CG",fb:"https://www.facebook.com/harmeet.saluja.16",phone:"7677294007", insta:"https://www.instagram.com/harmeet____/"},
-{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",fb:"https://www.facebook.com/profile.php?id=100007865385033",phone:"+919929919437", insta: "https://www.instagram.com/ujjwal18_/"},
+{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",fb:"https://www.facebook.com/profile.php?id=100007865385033",phone:"9929919437", insta: "https://www.instagram.com/ujjwal18_/"},
 ]
--- src/app/rules/rules-interface.ts
+++ src/app/rules/rules-interface.ts
@@ -11,8 +11,8 @@ let coverimgmobile="assets/midocs/coverimgrulesmobile.png";
 
 export const rulesList:rulesDescription[]=[
 {id:1, name: "Solo Dance",coverimg:"assets/midocs/coverimgrules.png",coverimgmobile:"assets/midocs/coverimgrules.jpg"},
-{id:2, name: "Creative Writing",coverimg:"assets/midocs/creative writing.jpg",coverimgmobile:"assets/midocs/creative writing.jpg"},
-{id:3, name: "Modelling",coverimg:"assets/midocs/modelling.jpg",coverimgmobile:"assets/midocs/modelling.jpg"},
-{id:4, name: "Solo Singing",coverimg:"assets/midocs/solo singing.jpg",coverimgmobile:"assets/midocs/solo singing.jpg"},
-{id:5, name: "Rap",coverimg:"assets/midocs/rap.jpg",coverimgmobile:"assets/midocs/rap.jpg"},
+{id:2, name: "Creative Writing",coverimg:"assets/midocs/creative writing cover.jpg",coverimgmobile:"assets/midocs/creative writing cover.jpg"},
+{id:3, name: "Modelling",coverimg:"assets/midocs/modelling cover.jpg",coverimgmobile:"assets/midocs/modelling cover.jpg"},
+{id:4, name: "Solo Singing",coverimg:"assets/midocs/solo singing cover.jpg",coverimgmobile:"assets/midocs/solo singing cover.jpg"},
+{id:5, name: "Rap",coverimg:"assets/midocs/rap cover.jpg",coverimgmobile:"assets/midocs/rap cover.jpg"},
 ]
