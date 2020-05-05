873228cd92c8fd2a6b649e58e5bab7995b398c28
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-05-05 23:37:00 +0530

    fixed solo dance mobile cover image

--- src/app/rules/rules-interface.ts
+++ src/app/rules/rules-interface.ts
@@ -10,7 +10,7 @@ let coverimage="assets/midocs/coverimgrules.png";
 let coverimgmobile="assets/midocs/coverimgrulesmobile.png";
 
 export const rulesList:rulesDescription[]=[
-{id:1, name: "Solo Dance",coverimg:"assets/midocs/coverimgrules.png",coverimgmobile:"assets/midocs/coverimgrules.jpg"},
+{id:1, name: "Solo Dance",coverimg:"assets/midocs/coverimgrules.png",coverimgmobile:"assets/midocs/coverimgrulesmobile.png"},
 {id:2, name: "Creative Writing",coverimg:"assets/midocs/creative writing cover.jpg",coverimgmobile:"assets/midocs/creative writing cover.jpg"},
 {id:3, name: "Modelling",coverimg:"assets/midocs/modelling cover.jpg",coverimgmobile:"assets/midocs/modelling cover.jpg"},
 {id:4, name: "Solo Singing",coverimg:"assets/midocs/solo singing cover.jpg",coverimgmobile:"assets/midocs/solo singing cover.jpg"},
