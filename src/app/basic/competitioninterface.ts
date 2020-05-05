5fe30b74db9ba6e56d03992f9ceaaf91a094e05a
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-05-05 10:14:03 +0530

    Images added

--- src/app/basic/competitioninterface.ts
+++ src/app/basic/competitioninterface.ts
@@ -11,14 +11,14 @@ export interface Competition{
 let desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
 export const competitionsList:Competition[]=[
 {id:1,name:"Solo Dance",link:"",image:"assets/midocs/dance.jpeg",description:"Grab the moment and dance to live, to breathe, to be free, to be you!",state:"s1"},
-{id:2,name:"Creative Writing",link:"",image:"assets/midocs/violin.png",description:"If words for you are an expression of your soul, this is it!",state:"s1"},
-{id:3,name:"Modelling",link:"",image:"assets/midocs/violin.png",description:"It's becoming a POEM before a camera",state:"s1"},
-{id:4,name:"Solo Singing",link:"",image:"assets/midocs/dance.jpeg",description:"Let your voice transcend all the boundaries...",state:"s1"},
-{id:5,name:"Design",link:"",image:"assets/midocs/violin.png",description:"Design is intelligence made visible. Show us what you got!",state:"s1"},
-{id:6,name:"Rap",link:"",image:"assets/midocs/dance.jpeg",description:"Stay tuned for more competitions..",state:"s1"},
-{id:7,name:"Solo Instrumental",link:"",image:"assets/midocs/dance.jpeg",description:"Stay tuned for more competitions..",state:"s1"},
-{id:8,name:"Poetry",link:"",image:"assets/midocs/dance.jpeg",description:"Stay tuned for more competitions..",state:"s1"},
-{id:9,name:"Photography",link:"",image:"assets/midocs/dance.jpeg",description:"Stay tuned for more competitions..",state:"s1"},
+{id:2,name:"Creative Writing",link:"",image:"assets/midocs/poetry2.jpg",description:"If words for you are an expression of your soul, this is it!",state:"s1"},
+{id:3,name:"Modelling",link:"",image:"assets/midocs/modelling.jpg",description:"It's becoming a POEM before a camera",state:"s1"},
+{id:4,name:"Solo Singing",link:"",image:"assets/midocs/solosinging.jpg",description:"Let your voice transcend all the boundaries...",state:"s1"},
+{id:5,name:"Design",link:"",image:"assets/midocs/design.jpg",description:"Design is intelligence made visible. Show us what you got!",state:"s1"},
+{id:6,name:"Rap",link:"",image:"assets/midocs/rap.jpg",description:"Stay tuned for more competitions..",state:"s1"},
+{id:7,name:"Solo Instrumental",link:"",image:"assets/midocs/violin.png",description:"Stay tuned for more competitions..",state:"s1"},
+{id:8,name:"Poetry",link:"",image:"assets/midocs/poetry.jpg",description:"Stay tuned for more competitions..",state:"s1"},
+{id:9,name:"Photography",link:"",image:"assets/midocs/photography.jpg",description:"Stay tuned for more competitions..",state:"s1"},
 
 ]
 
