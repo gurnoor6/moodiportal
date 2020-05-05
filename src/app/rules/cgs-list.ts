97427f58b3313fb684f07ae5228da8139f8e101d
Anuj Agrawal <anujagrawal.official@gmail.com>
2020-05-05 22:00:13 +0530

    added social media links

--- src/app/rules/cgs-list.ts
+++ src/app/rules/cgs-list.ts
@@ -3,11 +3,12 @@ export interface CG{
 	name:string;
 	image:string;
 	description:string;
-	contact:string;
 	phone:string;
+	fb:string;
+	insta:string;
 } 
 
 export const cgs_list:CG[]=[
-{id:1,name:"Harmeet Singh",image:"assets/harmeet.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/harmeet.saluja.16",phone:"7677294007"},
-{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",contact:"https://www.facebook.com/profile.php?id=100007865385033",phone:"+919929919437"},
+{id:1,name:"Harmeet Singh",image:"assets/harmeet.jpeg",description:"Hi I am CG",fb:"https://www.facebook.com/harmeet.saluja.16",phone:"7677294007", insta:"https://www.instagram.com/harmeet____/"},
+{id:1,name:"Ujjwal Soni",image:"assets/ujjwal.jpeg",description:"Hi I am CG",fb:"https://www.facebook.com/profile.php?id=100007865385033",phone:"+919929919437", insta: "https://www.instagram.com/ujjwal18_/"},
 ]
